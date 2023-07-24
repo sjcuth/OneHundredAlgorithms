a37f08c4aa7846e871a7b5913bdafcc4ea3aa190
Scott <scottjcuthbertson@gmail.com>
2023-07-23 22:11:16 -0400

    Adding a second algorithm

---
+++ addTwoDigits.ts
@@ -0,0 +1,22 @@
+function add(param1: number, param2: number): number {
+
+    return param1 + param2;
+
+}
+
+console.log( add(1,9) );
+console.log( add(2,3) );
+
+function add2(...param1: number[]): number {
+    let total = 0;
+
+    param1.forEach((num) => {
+        total += num;
+    });
+
+    return total;
+
+}
+
+console.log( add2(1, 2, 3, 4, 5) );
+console.log( add2(2, 3) );
